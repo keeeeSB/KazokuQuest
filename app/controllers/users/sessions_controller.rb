0225b2fea85c6b6e736ad63297250d8e81266344
KeeeeSB <kfp.a@icloud.com>
2025-12-15 04:45:27 +0900

    ログイン後のフラッシュメッセージの追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,17 @@
 class Users::SessionsController < Devise::SessionsController
+  def create
+    super do |resource|
+      flash[:notice] = if resource.family_id.present?
+                         'ログインしました。'
+                       else
+                         '家族情報を登録してください。'
+                       end
+    end
+  end
+
   protected
 
   def after_sign_in_path_for(resource)
-    if resource.family_id.present?
-      set_flash_message!(:notice, :signed_in, message: 'ログインしました！')
-      root_path
-    else
-      set_flash_message!(:notice, :signed_in, message: '家族情報を登録してください。')
-      new_family_path
-    end
+    resource.family_id.present? ? root_path : new_family_path
   end
 end
