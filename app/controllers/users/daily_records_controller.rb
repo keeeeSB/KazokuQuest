5e8a7ee2cf5ab3ffc0587f5e8a3d63754d9d3b82
KeeeeSB <kfp.a@icloud.com>
2025-12-24 00:26:15 +0900

    今日の記録ページに日付指定ボタンを追加

--- app/controllers/users/daily_records_controller.rb
+++ app/controllers/users/daily_records_controller.rb
@@ -1,6 +1,6 @@
 class Users::DailyRecordsController < Users::ApplicationController
   def show
-    date = Date.current
+    date = params[:date]&.to_date || Date.current
     @daily_record = DailyRecord.new(current_user, date)
   end
 end
