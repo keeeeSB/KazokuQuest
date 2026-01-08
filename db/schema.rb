f6a2441dcc0070c7ea953c40bd3e553215d2fd3c
KeeeeSB <kfp.a@icloud.com>
2026-01-09 04:03:51 +0900

    Badgeに:rule_categoryを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_04_192310) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_06_184909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_04_192310) do
     t.boolean "enabled", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "rule_category"
     t.index ["name"], name: "index_badges_on_name", unique: true
   end
 
